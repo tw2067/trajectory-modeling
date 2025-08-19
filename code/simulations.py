06313d4fd1aa1332df169db621d82c62248e8755
tw2067 <tamarw1@mail.tau.ac.il>
2025-08-19 10:46:26 +0300

    generalize bayesian implementation

--- code/simulations.py
+++ code/simulations.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from bayesian_trajs import compute_time_varying_trajectory_covariates_parallel
+import os
+from bayesian_trajs import compute_time_varying_trajectory_covariates_parallel, pos_flags_from_traj
 
 def simulate_lab_data(n_patients=100, lab_tests=["eGFR"], seed=920):
     np.random.seed(seed)
@@ -37,9 +38,42 @@ def simulate_lab_data(n_patients=100, lab_tests=["eGFR"], seed=920):
 
 
 if __name__ == '__main__':
-    sim_labs = simulate_lab_data()
-    traj_covs = compute_time_varying_trajectory_covariates_parallel(sim_labs, n_samples=200, n_jobs=-1, verbose=5)
-    traj_covs.to_csv('test_res/traj_covs_sim.csv')
-    print(traj_covs)
-    # TODO: do simulations
+
+    # sim_labs = simulate_lab_data()
+    # traj_covs = compute_time_varying_trajectory_covariates_parallel(sim_labs, min_points_per_window=6, n_samples=400, tune=400, n_jobs=-1, verbose=5)
+    # traj_covs.to_csv('test_res/traj_covs_sim.csv')
+    # print(traj_covs)
+
+    updrs3_scores = pd.read_csv('../data/ppmi/parsed/updrs3_parsed.csv')
+    updrs3_on_scores = pd.read_csv('../data/ppmi/parsed/updrs3_on_parsed.csv')
+
+    traj_covs_off = compute_time_varying_trajectory_covariates_parallel(updrs3_scores,
+                                                                        window_years=9,
+                                                                        flat_thr= 0.5,
+                                                                        decline_thr=1,
+                                                                        nonlinear_gap=2,
+                                                                        min_points_per_window=8,
+                                                                        grid_freq=4,
+                                                                        n_samples=400,
+                                                                        tune=500,
+                                                                        pids='PATNO',
+                                                                        values='updrs3_score',
+                                                                        class_func=pos_flags_from_traj,
+                                                                        verbose=5)
+    traj_covs_off.to_csv('test_res/updrs3_off_covs.csv', index=False)
+
+    traj_covs_on = compute_time_varying_trajectory_covariates_parallel(updrs3_on_scores,
+                                                                        window_years=9,
+                                                                        flat_thr=0.5,
+                                                                        decline_thr=1,
+                                                                        nonlinear_gap=2,
+                                                                        min_points_per_window=8,
+                                                                        grid_freq=4,
+                                                                        n_samples=400,
+                                                                        tune=500,
+                                                                        pids='PATNO',
+                                                                        values='updrs3_score_on',
+                                                                        class_func=pos_flags_from_traj,
+                                                                        verbose=5)
+    traj_covs_on.to_csv('test_res/updrs3_on_covs.csv', index=False)
 
