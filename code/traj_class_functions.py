282111b57c194f38aa2772d150973f0cf9958a34
tw2067 <tamarw1@mail.tau.ac.il>
2025-08-24 17:17:13 +0300

    add mmse trajectory classification

--- code/traj_class_functions.py
+++ code/traj_class_functions.py
@@ -49,3 +49,29 @@ def pos_flags_from_traj(traj, time_grid, flat_thr=0.5, decline_thr=1, nonlinear_
         'linear_decline': frac_decl >= 0.8,
         'nonlinear': nonlinear
     }
+
+
+def mmse_flag_from_traj(traj, time_grid, flat_thr=-0.3 ,decline_thr=-1.5, nonlinear_gap=1,
+                        aging_thr = -0.5):
+    dt = np.diff(time_grid)
+    dy = np.diff(traj)
+    slopes = dy / dt
+
+    frac_flat = (slopes >= aging_thr).mean()
+    total_flat = np.mean(slopes) >= flat_thr
+
+    frac_slow = ((decline_thr <= slopes) & (slopes < aging_thr)).mean()
+    frac_fast = (decline_thr > slopes).mean()
+
+    s = np.sort(slopes)  # ascending (more negative first)
+    half = len(s) // 2
+    fast = s[:half].mean()  # faster decline half (more negative)
+    slow = s[half:].mean()  # slower half
+    nonlinear = abs(fast - slow) > nonlinear_gap
+
+    return {
+        'prolonged_nonprogression': (frac_flat >= 0.8) & total_flat,
+        'slow_linear_decline': frac_slow >= 0.8,
+        'fast_linear_decline': frac_fast >= 0.8,
+        'nonlinear': nonlinear
+    }
